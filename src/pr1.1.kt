758d5a10030dd63d8be8e67a758b9f6b2dd995b3
MOHAN SIVA RAO <mohansivaraot@gmail.com>
2025-11-26 21:37:07 +0530

    Initial commit

---
+++ src/pr1.1.kt
@@ -0,0 +1,23 @@
+fun main() {
+    // Different data type variables
+    val intValue: Int = 100
+    val doubleValue: Double = 99.99
+    val floatValue: Float = 12.34F
+    val longValue: Long = 123456789L
+    val shortValue: Short = 32000
+    val byteValue: Byte = 120
+    val charValue: Char = 'A'
+    val booleanValue: Boolean = true
+    val stringValue: String = "Hello, Kotlin!"
+
+    // Display values
+    println("Integer Value: $intValue")
+    println("Double Value: $doubleValue")
+    println("Float Value: $floatValue")
+    println("Long Value: $longValue")
+    println("Short Value: $shortValue")
+    println("Byte Value: $byteValue")
+    println("Char Value: $charValue")
+    println("Boolean Value: $booleanValue")
+    println("String Value: $stringValue")
+}
